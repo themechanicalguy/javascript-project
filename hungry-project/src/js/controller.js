083dd083bea239633f72794b108acfa5a4a38bc9
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2022-12-16 23:14:41 +0530

    js project setup

---
+++ hungry-project/src/js/controller.js
@@ -0,0 +1,41 @@
+const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const showRecipe = async () => {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    // console.log('-->', res);
+    const data = await res.json();
+    // console.log(data);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+    if (!data.ok) throw new Error(`${data.message} (${res.status})`);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+showRecipe();
